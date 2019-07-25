28567b99681a536438b530d914226e297b2dfea1
ian <ian@wherelabs.com>
2019-07-25 17:22:00 +0200

    HDL for logic analyzer debugging branch. enable both SRAMs, spi and quad read and write

--- hdl/iobuffer/buspirate.v
+++ hdl/iobuffer/buspirate.v
@@ -4,31 +4,34 @@
 //------------------------------------------------------------------
 `include "iobuf.v"
 `include "iobufphy.v"
+`include "synchronizer.v"
 module top #(
   parameter MC_DATA_WIDTH = 16,
-  parameter MC_ADD_WIDTH = 6
+  parameter MC_ADD_WIDTH = 6,
+  parameter LA_WIDTH = 8,
+  parameter LA_CHIPS = 2
+
 ) (
   input clock,
   inout bufio,
   output wire bufdir,
   output wire bufod,
-  output wire sram0_clock,
-  output wire sram0_cs,
-  inout[3:0] sram0_sio,
-  input wire mcu_quadmode, mcu_direction,
+  output wire[LA_CHIPS-1:0] sram_clock,
+  output wire[LA_CHIPS-1:0] sram_cs,
+  inout[LA_WIDTH-1:0] sram_sio,
+  output wire lat_oe,
+  input wire [LA_WIDTH-1:0] lat,
+  //input wire mcu_quadmode, mcu_direction,
   input wire mcu_clock,
   input wire mcu_cs,
   input wire mcu_mosi, //sio0
   output wire mcu_miso, //sio1
-  output wire lat_oe,
-  input wire [3:0] lat,
-
   input wire mc_oe, mc_ce, mc_we,
   input wire [MC_ADD_WIDTH-1:0] mc_add,
   inout [MC_DATA_WIDTH-1:0] mc_data
   );
     // memory regs
-    reg [MC_DATA_WIDTH-1:0] SRAM [32:0];
+    reg [MC_DATA_WIDTH-1:0] register [32:0];
     // Tristate pin handling
   	// Memory controller interface
   	wire [MC_DATA_WIDTH-1:0] mc_din;
@@ -45,49 +48,53 @@ module top #(
 
     reg [9:0] sample_counter;
 
-    wire [3:0] sram0_sio_tdi;
-    wire [3:0] sram0_sio_tdo;
+    wire [LA_WIDTH-1:0] sram_sio_tdi;
+    wire [LA_WIDTH-1:0] sram_sio_tdo;
 
     //                  oe      od  dir din dout       bufdir bufod      the pins from the SB_IO block below
     iobuf MOSI_BUF(1'b1, 1'b0, 1'b0, count[N],dout,    bufdir,  bufod,  buf_data_oe,buf_data_dout,buf_data_din);
 
-    //SRAM test:
+    //register test:
     //straight through SPI
-    reg [3:0] sram0_sio_tdo_d;
-    assign sram0_clock=mcu_clock;
-    assign sram0_cs=mcu_cs;
-    assign sram0_sio_tdo[0]=mcu_quadmode?SRAM[1][0]:mcu_mosi;
-    assign sram0_sio_tdo[3:1]=SRAM[1][3:1];
-    //assign sram0_sio_tdo=4'b0000;
-    assign mcu_miso=sram0_sio_tdi[1];
-    //assign sram0_sio_tdo=SRAM[1][3:0];
+    //reg [LA_WIDTH-1:0] sram_sio_tdo_d;
+    assign sram_clock[0]=register[2][0]?(register[2][1]?mc_we_sync:mc_oe_sync):mcu_clock;
+    assign sram_clock[1]=register[2][0]?(register[2][1]?mc_we_sync:mc_oe_sync):mcu_clock;
+    //assign sram_clock[0]=mcu_quadmode?(mcu_direction?mc_we_sync:mc_oe_sync):mcu_clock;
+    assign sram_cs[0]=register[2][0]||!register[2][2]?mcu_cs:1'b1;
+    assign sram_cs[1]=register[2][0]||register[2][2]?mcu_cs:1'b1;
+    assign sram_sio_tdo[0]=register[2][0]?register[1][0]:mcu_mosi;
+    assign sram_sio_tdo[4]=register[2][0]?register[1][4]:mcu_mosi;
+    assign {sram_sio_tdo[7:5],sram_sio_tdo[3:1]}={register[1][7:5],register[1][3:1]};
+    assign mcu_miso=register[2][2]?sram_sio_tdi[5]:sram_sio_tdi[1];
 
     //LATCH OE
     assign lat_oe=1'b0;
-    //assign mcu_sio_tdo=(mcu_arm) ? lat : sram0_sio_tdi;
-    //assign sram0_sio_tdo=(mcu_arm) ? lat : mcu_sio_tdi;
 
+    //wires tied to the memory controller WE and OE signals
+    wire mc_we_sync,mc_oe_sync,mc_ce_sync;
+    sync MC_WE_SYNC(clock, mc_we, mc_we_sync);
+    sync MC_OE_SYNC(clock, mc_oe, mc_oe_sync);
+    sync MC_CE_SYNC(clock, mc_ce, mc_ce_sync);
 
     always @(posedge clock)
       begin
         count <= count + 1;
-        //SRAM[1][3:0] <= sram0_sio_tdi;
         if(!mc_ce)
         begin
           if ((!mc_we))			// write (proper)
           begin
-            SRAM [mc_add] <= mc_din; //16'hFFFF;
+            register [mc_add] <= mc_din; //16'hFFFF;
           end
           else if ((mc_we))		// read
           begin
-            mc_dout_d <= SRAM [mc_add];
+            mc_dout_d <= register [mc_add];
           end
         end
-        else if(mcu_quadmode)
+        else if(register[2][0])
         begin
-          if(!mcu_direction)
+          if(!register[2][1])
           begin
-            SRAM[1][3:0]<=sram0_sio_tdi;
+            register[1][7:0]<=sram_sio_tdi;
           end
         end
       end
@@ -95,35 +102,35 @@ module top #(
 
 
     //define the tristate data pin explicitly in the top module
-    //SRAM MOSI
+    //register MOSI
     SB_IO #(
       .PIN_TYPE(6'b1010_01), //tristate
       .PULLUP(1'b0)          //no pullup
-    ) sram0mosi (
-      .PACKAGE_PIN(sram0_sio[0]),//which pin
-      .OUTPUT_ENABLE(mcu_quadmode?mcu_direction:1'b1),   //output enable wire mcu_quadmode?mcu_direction:1'b1
-      .D_OUT_0(sram0_sio_tdo[0]),        //data out wire
-      .D_IN_0(sram0_sio_tdi[0])           //data in wire
+    ) sram_mosi_sbio[LA_CHIPS-1:0] (
+      .PACKAGE_PIN({sram_sio[4],sram_sio[0]}),//which pin
+      .OUTPUT_ENABLE(register[2][0]?register[2][1]:1'b1),   //output enable wire mcu_quadmode?mcu_direction:1'b1
+      .D_OUT_0({sram_sio_tdo[4],sram_sio_tdo[0]}),        //data out wire
+      .D_IN_0({sram_sio_tdi[4],sram_sio_tdi[0]})           //data in wire
     );
-    //SRAM MISO
+    //register MISO
     SB_IO #(
 			.PIN_TYPE(6'b1010_01), //tristate
 			.PULLUP(1'b0)          //no pullup
-		) sram0miso(
-			.PACKAGE_PIN(sram0_sio[1]),//which pin
-			.OUTPUT_ENABLE(mcu_quadmode?mcu_direction:1'b0),   //output enable wire mcu_quadmode?mcu_direction:1'b0
-			.D_OUT_0(sram0_sio_tdo[1]),        //data out wire
-			.D_IN_0(sram0_sio_tdi[1])           //data in wire
+		) sram_miso_sbio[LA_CHIPS-1:0](
+			.PACKAGE_PIN({sram_sio[5],sram_sio[1]}),//which pin
+			.OUTPUT_ENABLE(register[2][0]?register[2][1]:1'b0),   //output enable wire mcu_quadmode?mcu_direction:1'b0
+			.D_OUT_0({sram_sio_tdo[5],sram_sio_tdo[1]}),        //data out wire
+			.D_IN_0({sram_sio_tdi[5],sram_sio_tdi[1]})           //data in wire
 		);
-    //SRAM SIO2/3
+    //register SIO2/3
     SB_IO #(
 			.PIN_TYPE(6'b1010_01), //tristate
 			.PULLUP(1'b0)          //no pullup
-		) sram0sio23[1:0] (
-			.PACKAGE_PIN(sram0_sio[3:2]),//which pin
-			.OUTPUT_ENABLE(mcu_quadmode&&mcu_direction ),   //quadmode = 1 and direction = 1
-			.D_OUT_0(sram0_sio_tdo[3:2]),        //data out wire
-			.D_IN_0(sram0_sio_tdi[3:2])           //data in wire
+		) sram_sio_sbio[3:0] (
+			.PACKAGE_PIN({sram_sio[7:6],sram_sio[3:2]}),//which pin
+			.OUTPUT_ENABLE(register[2][0]&&register[2][1] ),   //quadmode = 1 and direction = 1
+			.D_OUT_0({sram_sio_tdo[7:6],sram_sio_tdo[3:2]}),        //data out wire
+			.D_IN_0({sram_sio_tdi[7:6],sram_sio_tdi[3:2]})           //data in wire
 		);
 
 
@@ -153,38 +160,38 @@ module top #(
 
 
     initial begin
-      SRAM[6'b00000] <= 16'b0101101001011010;				// test values
-      SRAM[6'b00001] <= 16'b1010010110100101;
-      SRAM[6'b00010] <= 16'b0101101001011010;
-      SRAM[6'b00011] <= 16'b1010010110100101;
-      SRAM[6'b00100] <= 16'b0101101001011010;
-      SRAM[6'b00101] <= 16'b1010010110100101;
-      SRAM[6'b00110] <= 16'b0101101001011010;
-      SRAM[6'b00111] <= 16'b1010010110100101;
-      SRAM[6'b01000] <= 16'b0101101001011010;
-      SRAM[6'b01001] <= 16'b1010010110100101;
-      SRAM[6'b01010] <= 16'b0101101001011010;
-      SRAM[6'b01011] <= 16'b1010010110100101;
-      SRAM[6'b01100] <= 16'b0101101001011010;
-      SRAM[6'b01101] <= 16'b1010010110100101;
-      SRAM[6'b01110] <= 16'b0101101001011010;
-      SRAM[6'b01111] <= 16'b1010010110100101;
-      SRAM[6'b10000] <= 16'b0101101001011010;
-      SRAM[6'b10001] <= 16'b1010010110100101;
-      SRAM[6'b10010] <= 16'b0101101001011010;
-      SRAM[6'b10011] <= 16'b1010010110100101;
-      SRAM[6'b10100] <= 16'b0101101001011010;
-      SRAM[6'b10101] <= 16'b1010010110100101;
-      SRAM[6'b10110] <= 16'b0101101001011010;
-      SRAM[6'b10111] <= 16'b1010010110100101;
-      SRAM[6'b11000] <= 16'b0101101001011010;
-      SRAM[6'b11001] <= 16'b1010010110100101;
-      SRAM[6'b11010] <= 16'b0101101001011010;
-      SRAM[6'b11011] <= 16'b1010010110100101;
-      SRAM[6'b11100] <= 16'b0101101001011010;
-      SRAM[6'b11101] <= 16'b1010010110100101;
-      SRAM[6'b11110] <= 16'b0101101001011010;
-      SRAM[6'b11111] <= 16'b1010010110100101;
+      register[6'b00000] <= 16'b0101101001011010;				// test values
+      register[6'b00001] <= 16'b1010010110100101;
+      register[6'b00010] <= 16'b0101101001011010;
+      register[6'b00011] <= 16'b1010010110100101;
+      register[6'b00100] <= 16'b0101101001011010;
+      register[6'b00101] <= 16'b1010010110100101;
+      register[6'b00110] <= 16'b0101101001011010;
+      register[6'b00111] <= 16'b1010010110100101;
+      register[6'b01000] <= 16'b0101101001011010;
+      register[6'b01001] <= 16'b1010010110100101;
+      register[6'b01010] <= 16'b0101101001011010;
+      register[6'b01011] <= 16'b1010010110100101;
+      register[6'b01100] <= 16'b0101101001011010;
+      register[6'b01101] <= 16'b1010010110100101;
+      register[6'b01110] <= 16'b0101101001011010;
+      register[6'b01111] <= 16'b1010010110100101;
+      register[6'b10000] <= 16'b0101101001011010;
+      register[6'b10001] <= 16'b1010010110100101;
+      register[6'b10010] <= 16'b0101101001011010;
+      register[6'b10011] <= 16'b1010010110100101;
+      register[6'b10100] <= 16'b0101101001011010;
+      register[6'b10101] <= 16'b1010010110100101;
+      register[6'b10110] <= 16'b0101101001011010;
+      register[6'b10111] <= 16'b1010010110100101;
+      register[6'b11000] <= 16'b0101101001011010;
+      register[6'b11001] <= 16'b1010010110100101;
+      register[6'b11010] <= 16'b0101101001011010;
+      register[6'b11011] <= 16'b1010010110100101;
+      register[6'b11100] <= 16'b0101101001011010;
+      register[6'b11101] <= 16'b1010010110100101;
+      register[6'b11110] <= 16'b0101101001011010;
+      register[6'b11111] <= 16'b1010010110100101;
     end
 
 endmodule
